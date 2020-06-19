a767c9c42afb53e7837101ab50883c9d23e3faf9
GrytsenkoAndrey <andreyapg@gmail.com>
2020-06-19 19:31:19 +0300

    #2 from data added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace sortData
 {
-    partial class Form1
+    partial class frmSort
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.rbtnUp = new System.Windows.Forms.RadioButton();
+            this.rbtnDown = new System.Windows.Forms.RadioButton();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.chkTitle = new System.Windows.Forms.CheckBox();
+            this.chkSave = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(34, 26);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Статус:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Удаляем текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Кол-во сортируемых эл-в:";
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(175, 69);
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(58, 20);
+            this.numQuantity.TabIndex = 3;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(64, 122);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Загрузка";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // rbtnUp
+            // 
+            this.rbtnUp.AutoSize = true;
+            this.rbtnUp.Location = new System.Drawing.Point(37, 172);
+            this.rbtnUp.Name = "rbtnUp";
+            this.rbtnUp.Size = new System.Drawing.Size(107, 17);
+            this.rbtnUp.TabIndex = 5;
+            this.rbtnUp.TabStop = true;
+            this.rbtnUp.Text = "по возрастанию";
+            this.rbtnUp.UseVisualStyleBackColor = true;
+            // 
+            // rbtnDown
+            // 
+            this.rbtnDown.AutoSize = true;
+            this.rbtnDown.Location = new System.Drawing.Point(37, 200);
+            this.rbtnDown.Name = "rbtnDown";
+            this.rbtnDown.Size = new System.Drawing.Size(91, 17);
+            this.rbtnDown.TabIndex = 6;
+            this.rbtnDown.TabStop = true;
+            this.rbtnDown.Text = "по убыванию";
+            this.rbtnDown.UseVisualStyleBackColor = true;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(64, 227);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 7;
+            this.btnSort.Text = "Сортировка";
+            this.btnSort.UseVisualStyleBackColor = true;
+            // 
+            // chkTitle
+            // 
+            this.chkTitle.AutoSize = true;
+            this.chkTitle.Location = new System.Drawing.Point(37, 278);
+            this.chkTitle.Name = "chkTitle";
+            this.chkTitle.Size = new System.Drawing.Size(170, 17);
+            this.chkTitle.TabIndex = 8;
+            this.chkTitle.Text = "Добавить заголовок в файл";
+            this.chkTitle.UseVisualStyleBackColor = true;
+            // 
+            // chkSave
+            // 
+            this.chkSave.AutoSize = true;
+            this.chkSave.Location = new System.Drawing.Point(37, 346);
+            this.chkSave.Name = "chkSave";
+            this.chkSave.Size = new System.Drawing.Size(193, 17);
+            this.chkSave.TabIndex = 9;
+            this.chkSave.Text = "Сохранять в каталог программы";
+            this.chkSave.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Заголовок";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(36, 320);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(299, 20);
+            this.txtTitle.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(64, 377);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Сохранение";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(64, 415);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmSort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(360, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkSave);
+            this.Controls.Add(this.chkTitle);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.rbtnDown);
+            this.Controls.Add(this.rbtnUp);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "frmSort";
+            this.Text = "Сортировка массива";
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.RadioButton rbtnUp;
+        private System.Windows.Forms.RadioButton rbtnDown;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.CheckBox chkTitle;
+        private System.Windows.Forms.CheckBox chkSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace sortData
 {
-    public partial class Form1 : Form
+    public partial class frmSort : Form
     {
-        public Form1()
+        public frmSort()
         {
             InitializeComponent();
         }
